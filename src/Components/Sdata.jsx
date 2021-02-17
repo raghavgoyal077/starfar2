24cd7b2eb0e3ecf753ac970c0222cf284281e14b
Sparsh Aggarwal <heymesparsh@gmail.com>
2021-02-17 15:31:59 +0530

    another commit

--- src/Components/Sdata.jsx
+++ src/Components/Sdata.jsx
@@ -1,8 +1,9 @@
 import four from '../four.jpg';
 import six from '../six.jpg';
+import eight from '../eight.jpg';
 import five_one from '../five_one.jpg';
 import five_two from '../five_two.jpg';
-
+import five_three from '../five_three.jpg';
 
 const Sdata = [
   {
@@ -48,6 +49,28 @@ const Sdata = [
     ],
     linkMore:
       'https://www.amazon.in/dp/B08PKC3ZHF/ref=sr_1_4?dchild=1&keywords=Starfar&qid=1607078908&sr=8-4',
+  },
+  {
+    imgSrc: five_three,
+    title: 'StarFar 5.25" Subwoofer, 4ohm/160W',
+    det: [
+      'Big Magnet (90mm X 17mm).',
+      'A Quality product with Silver Chrome coated chasis',
+      'Subwoofer weight 1098gms',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-5-25-inches-Subwoofer-160Watts/dp/B08VX2BLLR/ref=sr_1_6?crid=3MFOT9J700WIX&dchild=1&keywords=starfar&qid=1613551955&sprefix=starfar%2Caps%2C969&sr=8-6',
+  },
+  {
+    imgSrc: eight,
+    title: 'StarFar 8" Subwoofer, 8ohm/150W',
+    det: [
+      'Very Powerful Magnet (90mm X 17mm).',
+      'Polypropylene rubber cone with rubber ring.',
+      'Gives Boom Bass & creates vibrations from audio systems.',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Subwoofer-8Ohms-150Watts/dp/B08VWVFHSB/ref=sr_1_3?crid=3MFOT9J700WIX&dchild=1&keywords=starfar&qid=1613551955&sprefix=starfar%2Caps%2C969&sr=8-3',
   },
 ];
 

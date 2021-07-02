3f3d5e4d50677445f97b8be77e8bbbb2f41e3fa6
Sparsh Aggarwal <heymesparsh@gmail.com>
2021-07-02 18:47:30 +0530

    hello i m back

--- src/Components/Sdata.jsx
+++ src/Components/Sdata.jsx
@@ -4,6 +4,11 @@ import eight from '../eight.jpg';
 import five_one from '../five_one.jpg';
 import five_two from '../five_two.jpg';
 import five_three from '../five_three.jpg';
+import star_1 from '../star_1.jpg';
+import star_2 from '../star_2.png';
+import star_3 from '../star_3.png';
+import star_4 from '../star_4.png';
+import star_5 from '../star_5.png';
 
 const Sdata = [
   {
@@ -71,6 +76,66 @@ const Sdata = [
     ],
     linkMore:
       'https://www.amazon.in/Starfar-inches-Subwoofer-8Ohms-150Watts/dp/B08VWVFHSB/ref=sr_1_3?crid=3MFOT9J700WIX&dchild=1&keywords=starfar&qid=1613551955&sprefix=starfar%2Caps%2C969&sr=8-3',
+  },
+  {
+    imgSrc: star_1,
+    title: 'Starfar 4 inches Subwoofer 4 Ohms/ 35 Watts',
+    det: [
+      'Heavy Magnet (72mm X 15mm).Make It Very Powerful.',
+      'Heavy Copper Four Layer 1 inch voice coil.',
+      'A Quality product with Black Powder coated chassis.',
+      'Gives Boom Bass & creates vibrations from audio systems.',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Subwoofer-Ohms-Watts/dp/B096ZFJD6X/ref=sr_1_9?dchild=1&keywords=STarfar&qid=1625227809&sr=8-9',
+  },
+  {
+    imgSrc: star_2,
+    title: 'Starfar 5.25 inches Subwoofer 8 Ohms/160Watts/90mmMagnet',
+    det: [
+      'Heavy Magnet (90mm X 17mm).Make It Very Powerful.',
+      'Heavy copper voice coil (1") & chassis.',
+      'Copper coil on Kapton paper with metallic tags.',
+      'A Quality product with Black Powder coated chassis',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Subwoofer-160Watts-90mmMagnet/dp/B097QCWZ45/ref=sr_1_4?dchild=1&keywords=STarfar&qid=1625227809&sr=8-4',
+  },
+  {
+    imgSrc: star_3,
+    title: 'Starfar 6 inches Subwoofer 8Ohms/120Watts(Black)',
+    det: [
+      'Heavy Magnet (90mm X 17mm).Make It Very Powerful.',
+      'Heavy copper voice coil (1") & chassis.',
+      'Copper coil on Kapton paper with metallic tags.',
+      'Gives Boom Bass & creates vibrations from audio systems.',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Subwoofer-8Ohms-120Watts/dp/B097QDG39W/ref=sr_1_8?dchild=1&keywords=STarfar&qid=1625227809&sr=8-8',
+  },
+  {
+    imgSrc: star_4,
+    title: 'Starfar 8 inches Sub Woofer 8Ohms /150Watts',
+    det: [
+      'Heavy Magnet (90mm X 17mm).Make It Very Powerful.',
+      'Heavy copper voice coil (1") & chassis.',
+      'Copper coil on Kapton paper with metallic tags.',
+      'A Quality product with Black Powder coated Chassis.',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Woofer-8Ohms-150Watts/dp/B097QC4V11/ref=sr_1_2?dchild=1&keywords=STarfar&qid=1625227809&sr=8-2',
+  },
+  {
+    imgSrc: star_5,
+    title: 'Starfar 8 inches Subwoofer 8Ohms /200Watts',
+    det: [
+      'Heavy Magnet (120mm X 20mm).Make I.t Very Powerful.',
+      'Heavy copper voice coil (1.25") & chassis.',
+      'Copper coil on Kapton paper with metallic tags.',
+      'Gives Boom Bass & creates vibrations from audio systems.',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Subwoofer-8Ohms-200Watts/dp/B098248GB6/ref=sr_1_15?dchild=1&keywords=STarfar&qid=1625227809&sr=8-15',
   },
 ];
 

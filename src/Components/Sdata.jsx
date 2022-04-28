63af64fd7c1455951fd3fb363a7def9f42d02265
heymesparsh <71241578+heymesparsh@users.noreply.github.com>
2022-04-28 14:15:53 +0530

    final commit

--- src/Components/Sdata.jsx
+++ src/Components/Sdata.jsx
@@ -1,82 +1,14 @@
-import four from '../four.jpg';
-import six from '../six.jpg';
-import eight from '../eight.jpg';
-import five_one from '../five_one.jpg';
-import five_two from '../five_two.jpg';
-import five_three from '../five_three.jpg';
 import star_1 from '../star_1.jpg';
 import star_2 from '../star_2.jpg';
 import star_3 from '../star_3.jpg';
 import star_4 from '../star_4.jpg';
-import star_5 from '../star_5.jpg';
+import star_6 from '../star6.png';
+import star_7 from '../star7.png';
+
+
 
 const Sdata = [
-  {
-    imgSrc: four,
-    title: 'StarFar 4" Subwoofer, 4ohm/35W',
-    det: [
-      'Heavy Magnet (72mm X 15mm).',
-      'Heavy copper voice coil (1") & chasis.',
-      'Copper coil on Kapton paper with metallic tags.',
-    ],
-    linkMore:
-      'https://www.amazon.in/dp/B08PKW1NBC/ref=sr_1_1?dchild=1&keywords=Starfar&qid=1607078584&sr=8-1',
-  },
-  {
-    imgSrc: six,
-    title: 'StarFar 6" Subwoofer, 8ohm/120W',
-    det: [
-      'Very Powerful Magnet (90mm X 17mm).',
-      'Polypropylene rubber cone with rubber ring.',
-      'Gives Boom Bass & creates vibrations from audio systems.',
-    ],
-    linkMore:
-      'https://www.amazon.in/dp/B08PKGKJXX/ref=sr_1_3?dchild=1&keywords=Starfar&qid=1607078584&sr=8-3',
-  },
-  {
-    imgSrc: five_one,
-    title: 'StarFar 5.25" Subwoofer, 4ohm/90W',
-    det: [
-      'Big Magnet (80mm X 15mm).',
-      'A Quality product with Silver Chrome coated chasis',
-      'Subwoofer weight 998gms',
-    ],
-    linkMore:
-      'https://www.amazon.in/dp/B08PK95PPB/ref=sr_1_5?dchild=1&keywords=Starfar&qid=1607078584&sr=8-5',
-  },
-  {
-    imgSrc: five_two,
-    title: 'StarFar 5.25" Subwoofer, 4ohm/90W',
-    det: [
-      'Big Magnet (72mm X 15mm).',
-      'A Quality product with Silver Chrome coated chasis',
-      'Subwoofer weight 998gms',
-    ],
-    linkMore:
-      'https://www.amazon.in/dp/B08PKC3ZHF/ref=sr_1_4?dchild=1&keywords=Starfar&qid=1607078908&sr=8-4',
-  },
-  {
-    imgSrc: five_three,
-    title: 'StarFar 5.25" Subwoofer, 4ohm/160W',
-    det: [
-      'Big Magnet (90mm X 17mm).',
-      'A Quality product with Silver Chrome coated chasis',
-      'Subwoofer weight 1098gms',
-    ],
-    linkMore:
-      'https://www.amazon.in/Starfar-5-25-inches-Subwoofer-160Watts/dp/B08VX2BLLR/ref=sr_1_6?crid=3MFOT9J700WIX&dchild=1&keywords=starfar&qid=1613551955&sprefix=starfar%2Caps%2C969&sr=8-6',
-  },
-  {
-    imgSrc: eight,
-    title: 'StarFar 8" Subwoofer, 8ohm/150W',
-    det: [
-      'Very Powerful Magnet (90mm X 17mm).',
-      'Polypropylene rubber cone with rubber ring.',
-      'Gives Boom Bass & creates vibrations from audio systems.',
-    ],
-    linkMore:
-      'https://www.amazon.in/Starfar-inches-Subwoofer-8Ohms-150Watts/dp/B08VWVFHSB/ref=sr_1_3?crid=3MFOT9J700WIX&dchild=1&keywords=starfar&qid=1613551955&sprefix=starfar%2Caps%2C969&sr=8-3',
-  },
+  
   {
     imgSrc: star_1,
     title: 'Starfar 4 inches Subwoofer 4 Ohms/ 35 Watts',
@@ -88,6 +20,18 @@ const Sdata = [
     ],
     linkMore:
       'https://www.amazon.in/Starfar-inches-Subwoofer-Ohms-Watts/dp/B096ZFJD6X/ref=sr_1_9?dchild=1&keywords=STarfar&qid=1625227809&sr=8-9',
+  },
+  {
+    imgSrc: star_7,
+    title: 'Starfar 5.25 inches Subwoofer 4 Ohms/90Watts/80mmMagnet',
+    det: [
+      'Heavy Magnet (80mm X 15mm).Make It Very Powerful.',
+      'Heavy copper voice coil (1") & chassis.',
+      'Copper coil on Kapton paper with metallic tags.',
+      'A Quality product with Black Powder coated chassis',
+    ],
+    linkMore:
+      'https://www.amazon.in/Starfar-inches-Subwoofer-90Watts-80mmMagnet/dp/B09K814K58/ref=sr_1_1?crid=1VA7LGW30GFME&keywords=starfar+subwoofer&qid=1649851733&sprefix=%2Caps%2C208&sr=8-1',
   },
   {
     imgSrc: star_2,
@@ -126,7 +70,7 @@ const Sdata = [
       'https://www.amazon.in/Starfar-inches-Woofer-8Ohms-150Watts/dp/B097QC4V11/ref=sr_1_2?dchild=1&keywords=STarfar&qid=1625227809&sr=8-2',
   },
   {
-    imgSrc: star_5,
+    imgSrc: star_6,
     title: 'Starfar 8 inches Subwoofer 8Ohms /200Watts',
     det: [
       'Heavy Magnet (120mm X 20mm).Make I.t Very Powerful.',
